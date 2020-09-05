dd6ff42ba7db6029f3f64f8753430c17c9df60bf
Tanattha <tanattha.thuenim@gmail.com>
2020-09-05 14:46:53 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
+import { join } from 'path';
+import express, { static } from 'express';
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
