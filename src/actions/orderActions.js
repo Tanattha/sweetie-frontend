6bfd2c0ee32e2589dddd02cce6e804a8941ffdfc
Tanattha <tanattha.thuenim@gmail.com>
2020-09-15 09:55:29 -0400

    make changes of mDTP

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,21 +1,19 @@
-import { CREATE_ORDER  } from "./types";
+import { CREATE_ORDER } from "./types";
 import { USERS_URL } from "../config";
 
-export const createOrder = (order) => (dispatch) => {
-  fetch(USERS_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(order),
-  })
-
-  .then((res) => res.json())
-  .then((data) => {
-    dispatch({ type: CREATE_ORDER, payload: data });
-   
-  });
-    
+export const createOrder = (order) => {
+  return (dispatch) => {
+    fetch(USERS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: CREATE_ORDER, payload: data });
+      });
+  };
 };
-
