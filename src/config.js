d2edf010ce43022e319807dff6accff3a8da6e3f
Tanattha <tanattha.thuenim@gmail.com>
2020-09-30 16:10:55 -0400

    changed base url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//export const BASE_URL = "http://127.0.0.1:3000"
+
 export const BASE_URL = "https://sweetie-backend.herokuapp.com"
 export const PRODUCTS_URL = `${BASE_URL}/products`
 export const USERS_URL = `${BASE_URL}/users`
